3a0f55b65cda0314b42074bb048299167be00e3a
litchiny <litchiny@gmail.com>
2020-08-02 16:51:52 +0800

    2. 修改UI，实现方式1

--- app/src/main/java/com/litchiny/jetpackdemo/MainActivity.kt
+++ app/src/main/java/com/litchiny/jetpackdemo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.litchiny.jetpackdemo
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -17,16 +18,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.nav_fragment)
-        appBarConfiguration = AppBarConfiguration(navGraph = navController.graph)
-        setupActionBarWithNavController(navController,appBarConfiguration)
+        //设置Toolbar与Navigation绑定
+//        appBarConfiguration = AppBarConfiguration(navGraph = navController.graph)
+//        setupActionBarWithNavController(navController,appBarConfiguration)
 
         findViewById<BottomNavigationView>(R.id.bottom_nav)
             .setupWithNavController(navController)
     }
 
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
+    //点击Toolbar的back icon 返回上一级
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_fragment)
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
 }
