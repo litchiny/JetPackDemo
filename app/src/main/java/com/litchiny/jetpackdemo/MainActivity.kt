6ce2ed45b0019b61570c187c8be7361cff13bbb9
litchiny <litchiny@gmail.com>
2020-08-02 15:19:21 +0800

    首次提交

---
+++ app/src/main/java/com/litchiny/jetpackdemo/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.litchiny.jetpackdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration:AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_fragment)
+        appBarConfiguration = AppBarConfiguration(navGraph = navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
